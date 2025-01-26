e9385d30d34cbaec9310e57a273747c89b509f10
George Thomas <georgefsthomas@gmail.com>
2025-01-26 20:55:45 +0000

    Revert "Avoid HTML extension for Monpad URL"
    This reverts commit 09cd0aede98568e7536ac2ac3de950e54a9ef029.
    
    It doesn't really buy us anything, since we only use that path in an iframe. I'd rather just keep the code simple. Also, a nice side effect is that this way there's no overlap between Shake rules, so we needn't think about specificity.

--- Build.hs
+++ Build.hs
@@ -76,7 +76,7 @@ main = shakeArgs shakeOpts do
     (outDir </> stylesheet) %> \p -> do
         copyFileChanged stylesheet p
 
-    (outDir </> "monpad/index.html") %> \_ -> do
+    (outDir </> "monpad.html") %> \_ -> do
         -- TODO this needs to be re-run when the submodule hash changes
         -- for now we must manually run `./Build.hs dist/monpad.html -B`
         command_
@@ -91,7 +91,7 @@ main = shakeArgs shakeOpts do
             , "--login"
             , "/dev/null"
             , "--main"
-            , outDir </> "monpad/index.html"
+            , outDir </> "monpad.html"
             , "--json"
             , "--layout"
             , "((./monpad/dhall/lib/map-layout.dhall).void ./monpad/dhall/default.dhall)"
@@ -113,7 +113,6 @@ main = shakeArgs shakeOpts do
                                 TagOpen _ as ->
                                     find ((== "src") . fst) as <&> \(_, src) ->
                                         outDir </> T.unpack (T.takeWhile (/= '?') src)
-                                            </> "index.html"
                                 _ -> Nothing
                         pure $ RawBlock format t
                     block ->
