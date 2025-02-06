1371048ff0f77120e32c6f93850248c28db5275a
George Thomas <georgefsthomas@gmail.com>
2025-02-06 23:34:43 +0000

    Add margins

--- Build.hs
+++ Build.hs
@@ -336,7 +336,7 @@ addCommonHtml noDep body = do
             sequence_ $
                 links <&> \(p, t) ->
                     H.a (H.string t) ! HA.href (H.stringValue ("/" <> p)) ! HA.class_ "button-link"
-        body & foldMap \(b, t) -> H.div b ! HA.id "content" ! HA.class_ (H.textValue $ T.dropWhileEnd (== '/') t)
+        body & foldMap \(b, t) -> H.div (H.div b) ! HA.id "content" ! HA.class_ (H.textValue $ T.dropWhileEnd (== '/') t)
   where
     links =
         [ ("posts", "Blog")
